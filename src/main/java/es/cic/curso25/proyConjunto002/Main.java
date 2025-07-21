070652471600ec298c72fd049e6ace7689695222
rdpablos <rdpablos@formacion.cic.es>
2025-07-21 12:43:38 +0200

    primer commit del proyecto vacío

---
+++ src/main/java/es/cic/curso25/proyConjunto002/Main.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyConjunto002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso25/proyConjunto002/MainTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyConjunto002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
