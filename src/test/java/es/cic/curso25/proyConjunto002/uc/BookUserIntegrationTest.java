8cbca03dcb2e919cbacae3e5915a0c173d7f6c97
noname <mail@example.mail>
2025-07-23 12:37:02 +0200

    comit por la comitacion

--- src/test/java/es/cic/curso25/proyConjunto002/uc/BookUserIntegrationTest.java
+++ src/test/java/es/cic/curso25/proyConjunto002/uc/BookUserIntegrationTest.java
@@ -1,9 +1,58 @@
 package es.cic.curso25.proyConjunto002.uc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import es.cic.curso25.proyConjunto002.model.Book;
+import es.cic.curso25.proyConjunto002.repository.BookRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class BookUserIntegrationTest {
-
     
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void makeRelationUserBook() throws Exception{
+        Book book=new Book();
+        book.setAutor("Alguno");
+        book.setGenre("Misterio");
+        book.setIsbn("A123456789");
+        book.setPrice(9.99);
+        book.setTitle("El misterio de alguno");
+
+        String jsonBook=objectMapper.writeValueAsString(book);
+
+        mockMvc.perform(post("/book")
+                .contentType("application/json")
+                .content(jsonBook))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(result -> {
+                    String respuesta = result.getResponse().getContentAsString();
+                    Book registroCreado=objectMapper.readValue(respuesta, Book.class);
+                    
+                    Optional<Book> registroCreadoRealmente=bookRepository.findById(registroCreado.getId());
+                    assertTrue(registroCreadoRealmente.isPresent());
+                });
+    }
+
 }
